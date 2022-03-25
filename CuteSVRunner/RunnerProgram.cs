989cad04f3cd14f36cc26aae461feee9b672e0b4
Paige Ashlynn <mx.ashlynn@gmail.com>
2022-03-24 20:37:05 -0700

    Writes skeleton of smoke test.

--- CuteSVRunner/RunnerProgram.cs
+++ CuteSVRunner/RunnerProgram.cs
@@ -7,10 +7,34 @@ namespace CuteSVRunner
     /// </summary>
     internal static class RunnerProgram
     {
+        #region Test Routines
+        internal static bool TryLoadModels()
+            => true;
+
+        internal static bool TryLoadStatuses()
+            => true;
+
+        internal static bool TrySaveStatuses()
+            => true;
+
+        internal static bool TrySaveModels()
+            => true;
+        #endregion
+
         /// <summary>
         /// A simple program used to run some basic features of the <see cref="CuteSV"/>.
         /// </summary>
         internal static void Main()
-            => Console.WriteLine("Hello world!");
+        {
+            Console.WriteLine("Beginning smoke test.");
+
+            Console.WriteLine(TryLoadModels() ? "Loaded models." : "Failed to load models!");
+            Console.WriteLine(TryLoadStatuses() ? "Loaded statuses." : "Failed to load statuses!");
+
+            Console.WriteLine(TrySaveStatuses() ? "Saved game state." : "Failed to save statuses!");
+            Console.WriteLine(TrySaveModels() ? "Saved models." : "Failed to save models!");
+
+            Console.WriteLine("Test complete.");
+        }
     }
 }
