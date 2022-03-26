cf4ef691ddd8d8fb476cf7a5282dfb1fb7a09fa9
Paige Ashlynn <mx.ashlynn@gmail.com>
2022-03-25 20:21:42 -0700

    Further sketched in the smoke test's skeleton.

--- CuteSVRunner/RunnerProgram.cs
+++ CuteSVRunner/RunnerProgram.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace CuteSVRunner
 {
@@ -7,18 +8,66 @@ namespace CuteSVRunner
     /// </summary>
     internal static class RunnerProgram
     {
+        #region Test Objects
+        /// <summary>A container for simple test objects.</summary>
+        private static IList<object> SimpleObjects = new List<object>();
+
+        /// <summary>A container for complex test objects.</summary>
+        private static IList<object> ComplexObjects = new List<object>();
+        #endregion
+
         #region Test Routines
-        internal static bool TryLoadModels()
-            => true;
+        /// <summary>
+        /// Attempts to load a series of simple objects from a single file.
+        /// </summary>
+        /// <returns><c>true</c> if the objects were successfully deserialized; <c>false</c> otherwise.</returns>
+        private static bool TryLoadCollections()
+        {
+            SimpleObjects.Add(1);
+            SimpleObjects.Add(2);
+            SimpleObjects.Add(3);
 
-        internal static bool TryLoadStatuses()
-            => true;
+            return SimpleObjects.Count > 0;
+        }
 
-        internal static bool TrySaveStatuses()
-            => true;
+        /// <summary>
+        /// Attempts to load a series of complex objects from several files.
+        /// </summary>
+        /// <returns><c>true</c> if the objects were successfully deserialized; <c>false</c> otherwise.</returns>
+        private static bool TryLoadComplexObjects()
+        {
+            ComplexObjects.Add(1);
+            ComplexObjects.Add(2);
+            ComplexObjects.Add(3);
 
-        internal static bool TrySaveModels()
-            => true;
+            return ComplexObjects.Count > 0;
+        }
+
+        /// <summary>
+        /// Attempts to save a series of complex objects to several files.
+        /// </summary>
+        /// <returns><c>true</c> if the objects were successfully serialized; <c>false</c> otherwise.</returns>
+        private static bool TrySaveComplexObjects()
+        {
+            ComplexObjects.Remove(1);
+            ComplexObjects.Remove(2);
+            ComplexObjects.Remove(3);
+
+            return ComplexObjects.Count == 0;
+        }
+
+        /// <summary>
+        /// Attempts to save a series of simple objects to a single file.
+        /// </summary>
+        /// <returns><c>true</c> if the objects were successfully serialized; <c>false</c> otherwise.</returns>
+        private static bool TrySaveCollections()
+        {
+            SimpleObjects.Remove(1);
+            SimpleObjects.Remove(2);
+            SimpleObjects.Remove(3);
+
+            return SimpleObjects.Count == 0;
+        }
         #endregion
 
         /// <summary>
@@ -29,11 +78,11 @@ namespace CuteSVRunner
             Console.WriteLine("Beginning smoke test.");
             Console.WriteLine();
 
-            Console.WriteLine(TryLoadModels() ? "Loaded models." : "Failed to load models!");
-            Console.WriteLine(TryLoadStatuses() ? "Loaded statuses." : "Failed to load statuses!");
+            Console.WriteLine(TryLoadCollections() ? "Loaded simple objects." : "Failed to load simple objects!");
+            Console.WriteLine(TryLoadComplexObjects() ? "Loaded complex objects." : "Failed to load complex objects!");
 
-            Console.WriteLine(TrySaveStatuses() ? "Saved game state." : "Failed to save statuses!");
-            Console.WriteLine(TrySaveModels() ? "Saved models." : "Failed to save models!");
+            Console.WriteLine(TrySaveComplexObjects() ? "Saved complex objects." : "Failed to save complex objects!");
+            Console.WriteLine(TrySaveCollections() ? "Saved simple objects." : "Failed to save simple objects!");
 
             Console.WriteLine();
             Console.WriteLine("Test complete.");
